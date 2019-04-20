856d316c9951229c259e2b40a1b028045d454c50
KeVon Ticer <kevtice15@gmail.com>
2019-04-20 15:42:43 -0700

    FIxed bug where you couldn't switch to tabs in the same window

--- popup.js
+++ popup.js
@@ -4,22 +4,6 @@
 
 'use strict';
 
-// let changeColor = document.getElementById('changeColor');
-// chrome.storage.sync.get('color', function(data) {
-//   changeColor.style.backgroundColor = data.color;
-//   changeColor.setAttribute('value', data.color);
-// });
-
-// changeColor.onclick = function(element) {
-//   let color = element.target.value;
-//   chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-//     chrome.tabs.executeScript(
-//         tabs[0].id,
-//         {code: 'document.body.style.backgroundColor = "' + color + '";'});
-//   });
-// };
-
-
 //Get the tabs that matcht the google docs url
 chrome.tabs.query({url: "*://docs.google.com/*"}, function(results) {
   //console.log(results);
@@ -46,44 +30,64 @@ chrome.storage.sync.get('docsTabs', function(data){
     var docTabList = document.createElement("ul");
     for(var item of data.docsTabs){
       console.log("Daters:", item);
+      //Make the li tag
       var newLi = document.createElement("li");
+      //Make the favicon img tag, set it to the favicon, give it a css class
       var newFavicon = document.createElement("img");
       newFavicon.setAttribute("src", item.favIconUrl);
       newFavicon.className = "docFavicon";
+
+      //Make the div that will hold the tab title
       var newP = document.createElement("div");
-      console.log(newFavicon);
-      console.log(item.favIconUrl);
-      console.log("New favicon after attribute: ", newFavicon);
+      newP.className="docTextDiv";
+
+      //Make the button that will trigger the window and tab switch, put the window and tab ids on the tag  and give it a css class
       var newItem = document.createElement("button");
       newItem.className="docListItem";
       newItem.setAttribute("data-tabId", item.id);
       newItem.setAttribute("data-windowId", item.windowId);
+      //Put the favicon in the button
       newItem.append(newFavicon);
+      //Put the title of the tab in the div
       newP.innerHTML = (item.title);
-      newP.className="docTextDiv";
+      //Put the div in the button
       newItem.append(newP);
+      //Put the button in the li
       newLi.append(newItem);
+      //Put the li in the ul
       docTabList.append(newLi);
     }
+    //Put the ul in the overall container div.
     container.append(docTabList);
   }
 });
 
 
 document.addEventListener('click', function buttonClicked (event){
+  console.log("Is anything happening?:", event);
   if(event.target.matches('.docListItem')){
     console.log(typeof event.target.dataset.tabid);
     console.log(event.target.dataset.tabid);
     var tabIdNo = parseInt(event.target.dataset.tabid);
     var windowIdNo = parseInt(event.target.dataset.windowid);
-    chrome.windows.getLastFocused(function(oldWindow){
-      console.log("Old Window: ", oldWindow);
-        chrome.windows.update(windowIdNo, {focused: true}, function(window){
-          console.log(window);
+      chrome.windows.getCurrent({}, function(window){
+        console.log("Inner Window: ", window);
+
+        //if the tab is in the current window, just change the tab
+        if(windowIdNo === window.id){
           chrome.tabs.update(tabIdNo, {active: true}, function(tab){
             console.log(tab);
           });
-        });
+        }
+        //if different window, change window then change tab
+        else{
+          chrome.windows.update(windowIdNo, {focused: true}, function(window){
+            console.log(window);
+            chrome.tabs.update(tabIdNo, {active: true}, function(tab){
+              console.log(tab);
+            });
+          }); 
+        }
     });
   }
 });
