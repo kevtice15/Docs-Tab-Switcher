aeb2ca9345fa2d74ae2adbacfa2e22e99a29c847
KeVon T <kevtice15@gmail.com>
2019-05-13 15:15:21 -0700

    Added close buttons to each row that close the tab and reloads it so the list changes.

--- popup.js
+++ popup.js
@@ -30,19 +30,28 @@ chrome.tabs.query({url: "*://docs.google.com/*"}, function(results) {
       var newP = document.createElement("div");
       newP.className="docTextDiv";
 
-      //Make the button that will trigger the window and tab switch, put the window and tab ids on the tag  and give it a css class
+      //Make the button that will trigger the window and tab switch
       var newItem = document.createElement("button");
       newItem.className="docListItem";
-      newItem.setAttribute("data-tabId", item.id);
-      newItem.setAttribute("data-windowId", item.windowId);
+
       //Put the favicon in the button
       newItem.append(newFavicon);
       //Put the title of the tab in the div
       newP.innerHTML = (item.title);
       //Put the div in the button
       newItem.append(newP);
-      //Put the button in the li
+
+      //Put the button in the li, put the window and tab ids on the tag  and give it a css class
       newLi.append(newItem);
+      newLi.setAttribute("data-tabId", item.id);
+      newLi.setAttribute("data-windowId", item.windowId);
+
+      var closeButton = document.createElement("button");
+      closeButton.innerHTML = "Close";
+      closeButton.id = "close-button"; 
+      newLi.append(closeButton);
+
+
       //Put the li in the ul
       docTabList.append(newLi);
     }
@@ -53,11 +62,15 @@ chrome.tabs.query({url: "*://docs.google.com/*"}, function(results) {
 
 document.addEventListener('click', function buttonClicked (event){
   //console.log("Is anything happening?:", event);
+  console.log("TYPEOF:", typeof event.target.dataset.tabid);
+  console.log(event);
+  var tabIdNo = parseInt(event.target.parentNode.dataset.tabid);
+  var windowIdNo = parseInt(event.target.parentNode.dataset.windowid);
   if(event.target.matches('.docListItem')){
-    // console.log(typeof event.target.dataset.tabid);
+    console.log(typeof event.target.dataset.tabid);
     // console.log(event.target.dataset.tabid);
-    var tabIdNo = parseInt(event.target.dataset.tabid);
-    var windowIdNo = parseInt(event.target.dataset.windowid);
+    // var tabIdNo = parseInt(event.target.dataset.tabid);
+    // var windowIdNo = parseInt(event.target.dataset.windowid);
       chrome.windows.getCurrent({}, function(w){
         //console.log("Inner Window: ", w);
 
@@ -73,12 +86,54 @@ document.addEventListener('click', function buttonClicked (event){
             // console.log(w);
             chrome.tabs.update(tabIdNo, {active: true}, function(tab){
               //console.log(tab);
-              window.close();
+              
             });
           }); 
         }
     });
   }
+  else if (event.target.matches('#close-button')){
+    chrome.tabs.remove(tabIdNo);
+    location.reload();
+    //TODO: something if the tab doesn't exist
+  }
 });
 
 
+
+// document.addEventListener('mouseover', function hoveredButton(event) {
+//   console.log("Enter:", event);
+//   if (event.target.matches('.docListItem')){
+//     console.log(event.target);
+//     var closeButton = document.createElement("button");
+//     closeButton.innerHTML = "Close";
+//     closeButton.id = "close-button"; 
+//     //Add the close button after the target _as a sibling_
+//     event.target.parentNode.append(closeButton);
+    
+//   }
+// });
+
+// document.addEventListener('mouseout', function hoveredButton(event) {
+//   if (event.target.matches('.docListItem')){
+//     console.log("LEAVE:", event.target);
+//     var cB = document.getElementById("close-button");
+//     event.target.parentNode.removeChild(cB);
+//   }
+// });
+
+// document.addEventListener('mouseover', function overCloseButton(event){
+//   if(event.target.matches('#close-button')){
+//     console.log("IS THIS WORKING??");
+//   }
+// })
+
+// document.addEventListener('click', function closeClick(event) {
+//   if (event.target.matches('#close-button')){
+//     console.log("Close click");
+//     // var tabIdNo = parseInt(event.target.dataset.tabid);
+//     // chrome.tabs.remove(tabIdNo, function(tab){
+//     //   console.log ("Closed tab", tab);
+//     // })
+//   }
+// })
